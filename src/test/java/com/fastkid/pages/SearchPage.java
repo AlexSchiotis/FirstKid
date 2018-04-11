8e94c878d93ee1484cc2b29720b104b9d809465c
bianca <iepurerazvan@gmail.com>
2018-04-11 15:26:46 +0300

    w

--- src/test/java/com/fastkid/pages/SearchPage.java
+++ src/test/java/com/fastkid/pages/SearchPage.java
@@ -1,4 +1,17 @@
 package com.fastkid.pages;
 
-public class SearchPage {
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.annotations.DefaultUrl;
+import net.thucydides.core.pages.PageObject;
+import org.openqa.selenium.support.FindBy;
+
+@DefaultUrl("https://qa.firstkid.ro/")
+public class SearchPage extends PageObject{
+
+    @FindBy(id = "search")
+    private WebElementFacade searchField;
+
+    public void setSearchField() {
+
+    typeInto(searchField,"carucior");}
 }
--- src/test/java/com/fastkid/steps/serenity/SearchSteps.java
+++ src/test/java/com/fastkid/steps/serenity/SearchSteps.java
@@ -1,4 +1,23 @@
 package com.fastkid.steps.serenity;
 
-public class SearchSteps {
+import com.fastkid.features.search.SearchTest;
+import com.fastkid.pages.RegistryPage;
+import com.fastkid.pages.SearchPage;
+import net.thucydides.core.annotations.Step;
+import net.thucydides.core.steps.ScenarioSteps;
+
+public class SearchSteps extends ScenarioSteps {
+
+
+    SearchPage searchPage;
+
+    @Step
+    public void openPage() {
+        searchPage.open();
+    }
+
+    @Step
+    public void setSearchField() {
+        searchPage.setSearchField();
+    }
 }
